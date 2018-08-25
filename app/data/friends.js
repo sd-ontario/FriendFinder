2b22b4b1ac5a2ee1158e48263d61fbf1a39ece82
Stephen <stephen.dawe94@gmail.com>
2018-08-25 14:05:31 -0400

    Can't get results

---
+++ app/data/friends.js
@@ -0,0 +1,98 @@
+[
+    {
+    "name": "Superman",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/SupermanRoss.png/250px-SupermanRoss.png",
+    "scores": [
+    5,
+    1,
+    2,
+    4,
+    5,
+    2,
+    2,
+    5,
+    4,
+    1
+    ]
+    },
+    {
+    "name": "Bill",
+    "photo": "http://cdn3-www.craveonline.com/assets/mandatory/legacy/2013/04/man_file_1043888_1-bill-murray-funniest-actors-300x300.jpg",
+    "scores": [
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
+    ]
+    },
+    {
+    "name": "John",
+    "photo": "https://typeset-beta.imgix.net/rehost%2F2016%2F9%2F13%2Fbc05f04a-fb52-4c6f-915a-800da9ae021c.jpg",
+    "scores": [
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
+    ]
+    },
+    {
+    "name": "Jerry",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Jerry_Seinfeld_%281997%29.jpg/170px-Jerry_Seinfeld_%281997%29.jpg",
+    "scores": [
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
+    ]
+    },
+    {
+    "name": "George",
+    "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/7/70/George_Costanza.jpg/220px-George_Costanza.jpg",
+    "scores": [
+    5,
+    1,
+    4,
+    2,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
+    ]
+    },
+    {
+    "name": "Meep",
+    "photo": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/05/04/15/the-muppets-2.jpg?w968",
+    "scores": [
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    3,
+    3,
+    4,
+    1
+    ]
+    }
+    ]
